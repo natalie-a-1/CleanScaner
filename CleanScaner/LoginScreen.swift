6d40e609098b9fa6463c008dd44539f5677d30ef
nataliehill <natalie.a.hill-1@ou.edu>
2022-05-10 15:09:32 -0500

    LoginScreen buttons

--- CleanScaner/LoginScreen.swift
+++ CleanScaner/LoginScreen.swift
@@ -11,9 +11,7 @@ import AuthenticationServices
 
 struct LoginScreen: View {
     @AppStorage("email") var email: String = ""
-    @AppStorage("firstName") var firstName: String = ""
-    @AppStorage("lastName") var lastName: String = ""
-    @AppStorage("UserId") var UserId: String = ""
+    @AppStorage("password") var password: String = ""
     
     var body: some View {
         VStack() {
@@ -21,83 +19,52 @@ struct LoginScreen: View {
             Text("C L E A N  S C A N N E R")
             .frame(alignment: .center)
             .font(.largeTitle)
-            .padding()
-            Spacer()
+            .padding(.vertical, 120)
             
             VStack() {
-                Spacer()
                 HStack() {
                     Text("EMAIL:")
                         .font(.title3)
                         .padding()
-                //insert textbox for user
-                    Spacer()
+                        .padding(.trailing, -10)
+                    
+                    TextField("Enter email", text: $email)
+                        .padding()
+                        .padding(.leading, -10)
+                    
                 }
-                Spacer()
                 
                 HStack() {
                 Text("PASSWORD:")
                         .font(.title3)
                         .padding()
-                //insert textbox for user
+                        .padding(.trailing, -10)
+
+
                     Spacer()
+                    
+                    TextField("Enter password", text: $password)
+                        .padding()
+                        .padding(.leading, -10)
                 }
                 Spacer()
                 
-                Text("OR")
-                    .font(.title2)
-                    .padding()
-                Spacer()
-                 
-                NavigationView {
-                    VStack {
-                        if UserId.isEmpty {
-                            
-                        SignInWithAppleButton(.continue) { request in
-                            request.requestedScopes = [.email, .fullName]
-                        }
-                        
-                        onCompletion: { result in
-                            switch result {
-                            case .success(let auth):
-                                switch auth.credential {
-                                    case let credential as
-                                ASAuthorizationAppleIDCredential:
-                                    
-                                    let userId = credential.user
-                                    //below need ot be caught in backend or database because
-                                    //can not be gotten back
-                                    let email = credential.email
-                                    let firstName = credential.fullName?.givenName
-                                    let lastName = credential.fullName?.familyName
-                                    // TODO: add let for password
-                                    
-                                    self.email = email ?? ""
-                                    self.firstName = firstName ?? ""
-                                    self.lastName = lastName ?? ""
-                                    self.UserId = userId
-                                    break
-                                    
-                                default:
-                                    break
-                                }
-                                
-                            case.failure(let error):
-                                print(error)
-                            }
-                        }
-                        }
+//                Button("LOGIN") {
+//                    let localEmail: String = $email
+//                    email = localEmail
+//                    UserDefaults.standard.set(localEmail, forKey: "email")
+//
+//                    let localPass: String = $password
+//                    password = localPass
+//                    UserDefaults.standard.set(localPass, forKey: "password")
+//
+//                }
                     }
-                    .frame(width: 300, height: 50)
-                        .padding()
-                        .cornerRadius(10)
-                } .navigationTitle("Sign in")
+                }
 
                 
             }
         }
-    }
-}
 
 struct LoginScreen_Previews: PreviewProvider {
     static var previews: some View {
