3714b0992374943972ac6fdee6e58b1cfb5f2126
nataliehill <natalie.a.hill-1@ou.edu>
2022-05-10 15:10:38 -0500

    Created RegisterScreen

--- CleanScaner/RegisterScreen.swift
+++ CleanScaner/RegisterScreen.swift
@@ -5,16 +5,17 @@
 //  Created by Natalie Hill on 5/9/22.
 //
 
+//TODO: THIS WHOLE CLASS
 import SwiftUI
 
-struct SwiftUIView: View {
+struct RegisterScreen: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct SwiftUIView_Previews: PreviewProvider {
+struct RegisterScreen_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIView()
+        RegisterScreen()
     }
 }
