05d7654e7f076809478bb71629f8866d1992bc70
nataliehill <natalie.a.hill-1@ou.edu>
2022-05-10 15:11:05 -0500

    Edited settings

--- CleanScaner/CleanScanerApp.swift
+++ CleanScaner/CleanScanerApp.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct CleanScanerApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            OpeningScreen()
+            LoginScreen()
         }
     }
 }
